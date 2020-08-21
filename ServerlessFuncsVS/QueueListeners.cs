f4df494416caba68778822a3e17ca910ab1d450a
JoshGa <JoshGa@JOSHGA-PC>
2020-08-21 17:18:25 -0500

    first commit

---
+++ ServerlessFuncsVS/QueueListeners.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace ServerlessFuncsVS
+{
+    public static class QueueListeners
+    {
+        [FunctionName("QueueListeners")]
+        public static async Task Run(
+            [QueueTrigger("todos", Connection = "AzureWebJobsStorage")] Todo todo, 
+            [Blob("todos", Connection = "AzureWebJobsStorage")] CloudBlobContainer container,
+            ILogger log)
+        {
+            await container.CreateIfNotExistsAsync();
+
+            var blob = container.GetBlockBlobReference($"{todo.Id}.txt");
+
+            await blob.UploadTextAsync($"Created a new task: {todo.TaskDescription}");
+
+            log.LogInformation($"C# Queue trigger function processed: {todo.TaskDescription}");
+        }
+    }
+}
